bb69ca9b75e96931b6e7df26af7c2f6b33b6ba00
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-17 18:07:14 +0200

    clean up

--- src/components/AlbumsListContainer.jsx
+++ src/components/AlbumsListContainer.jsx
@@ -3,42 +3,14 @@ import AlbumsList from './AlbumsList'
 import { connect } from 'react-redux'
 import { addAlbum, setAlbums } from '../actions/albums'
 
-// const sleep = time => new Promise(
-//   resolve => setTimeout(() => resolve(`I waited for ${time} ms`), time)
-// )
 
 class AlbumsListContainer extends Component {
-  
+
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/albums')
       .then(response => response.json())
-      .then(myJson => this.props.setAlbums( myJson ))
+      .then(myJson => this.props.setAlbums(myJson))
       .catch(err => console.log(err))
-    
-    // sleep(2000)
-    //   .then(m => this.props.setAlbums([
-    //     {
-    //       id:1,
-    //       title: m
-    //     },
-    //     {
-    //       id:2,
-    //       title: 'album 2'
-    //     },
-    //     {
-    //       id:3,
-    //       title: 'last album'
-    //     }
-    //   ]))
-
-    // this.props.addAlbum({
-    //   id:5,
-    //   title: 'Enjoying sunshine'
-    // })
-    // this.props.addAlbum({
-    //   id:10,
-    //   title:'Having fun in the US'
-    // })
   }
 
   render() {
@@ -51,10 +23,10 @@ class AlbumsListContainer extends Component {
   }
 }
 
-export default connect(mapStateToProps, {addAlbum, setAlbums})(AlbumsListContainer)
+export default connect(mapStateToProps, { addAlbum, setAlbums })(AlbumsListContainer)
 
 function mapStateToProps(reduxState) {
-  return{
+  return {
     albums: reduxState.albums
   }
 }
