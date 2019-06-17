bccbe1d0c615a87e29a381613097193aaa4d21d8
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-17 18:06:41 +0200

    Async

--- src/components/AlbumsListContainer.jsx
+++ src/components/AlbumsListContainer.jsx
@@ -2,25 +2,34 @@ import React, { Component } from 'react'
 import AlbumsList from './AlbumsList'
 import { connect } from 'react-redux'
 import { addAlbum, setAlbums } from '../actions/albums'
-import { resolve } from 'path';
 
-const sleep = time => new Promise(
-  resolve => setTimeout(() => resolve(`I waited for ${time} ms`), time)
-)
+// const sleep = time => new Promise(
+//   resolve => setTimeout(() => resolve(`I waited for ${time} ms`), time)
+// )
 
 class AlbumsListContainer extends Component {
   
   componentDidMount() {
-    // fetch('https://jsonplaceholder.typicode.com/albums')
-    //   .then(response => response.json())
-    //   .then(myJson => this.setState({ albums: myJson }))
-    //   .catch(err => console.log(err))
+    fetch('https://jsonplaceholder.typicode.com/albums')
+      .then(response => response.json())
+      .then(myJson => this.props.setAlbums( myJson ))
+      .catch(err => console.log(err))
     
-    sleep(2000)
-      .then(message => this.props.addAlbum(1, message))
-
-    sleep(3000)
-      .then(m => this.props.addAlbum(2, m))
+    // sleep(2000)
+    //   .then(m => this.props.setAlbums([
+    //     {
+    //       id:1,
+    //       title: m
+    //     },
+    //     {
+    //       id:2,
+    //       title: 'album 2'
+    //     },
+    //     {
+    //       id:3,
+    //       title: 'last album'
+    //     }
+    //   ]))
 
     // this.props.addAlbum({
     //   id:5,
